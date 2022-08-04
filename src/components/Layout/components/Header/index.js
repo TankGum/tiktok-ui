a6510691d276458242bf6494034b9bc3f3b90a58
tankgum <aspirineqwe01@gmail.com>
2022-08-04 11:09:55 +0700

    9

--- src/components/Layout/components/Header/index.js
+++ src/components/Layout/components/Header/index.js
@@ -4,7 +4,6 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faCircleQuestion,
     faCircleXmark,
-    faCloudUpload,
     faCoins,
     faEarthAsia,
     faEllipsisVertical,
@@ -25,6 +24,8 @@ import images from '~/assets/images';
 import { Wrapper as PopperWrapper } from '~/components/Popper';
 import AccountItem from '~/components/AccountItem';
 import Menu from '~/components/Popper/Menu';
+import { InboxIcon, MessageIcon, UploadIcon } from '~/components/Icons';
+import Image from '~/components/Image';
 
 const cx = classNames.bind(styles);
 
@@ -135,7 +136,9 @@ function Header() {
                         <>
                             <Tippy delay={[0, 200]} content="Upload Videos" placement="bottom">
                                 <button className={cx('action-btn')}>
-                                    <FontAwesomeIcon icon={faCloudUpload} />
+                                    <UploadIcon />
+                                    <MessageIcon />
+                                    <InboxIcon />
                                 </button>
                             </Tippy>
                         </>
@@ -147,7 +150,7 @@ function Header() {
                     )}
                     <Menu items={currentUser ? userMenu : MENU_ITEMS} onChange={handleMenuChange}>
                         {currentUser ? (
-                            <img
+                            <Image
                                 className={cx('user-avatar')}
                                 src="https://p16-sign-va.tiktokcdn.com/tos-useast2a-avt-0068-giso/a7b022c19e6fe0d4184687610c92c902~c5_100x100.jpeg?x-expires=1659715200&x-signature=WEHdbSvUj9GGAhO20k2j7fRtRU8%3D"
                                 alt="TankGum"
